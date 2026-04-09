7a1542db20343f534d2d7c86652d0e146998de98
Lucas Barros <lucasbarros.al@gmail.com>
2026-04-09 06:48:38 -0300

    feat: add search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import Icon from '@react-native-vector-icons/feather';
+import { Colors } from '../theme/Colors';
+
+interface SearchBarProps {
+  placeholder?: string;
+  onChangeText?: (text: string) => void;
+  onSearch?: (text: string) => void;
+  debounceDelay?: number;
+}
+
+export function SearchBar({
+  placeholder = 'Search...',
+  onChangeText,
+  onSearch,
+  debounceDelay = 500,
+}: SearchBarProps) {
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onSearch?.(searchText);
+    }, debounceDelay);
+
+    return () => clearTimeout(timeout);
+  }, [searchText, debounceDelay, onSearch]);
+
+  const handleChangeText = (text: string) => {
+    setSearchText(text);
+    onChangeText?.(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconButton}>
+        <Icon name="search" size={20} color="gray" />
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor="gray"
+        value={searchText}
+        onChangeText={handleChangeText}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Colors.white,
+    borderRadius: 32,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    padding: 0,
+    marginLeft: 8,
+  },
+  iconButton: {
+    padding: 8,
+  },
+});
--- src/screens/history/History.tsx
+++ src/screens/history/History.tsx
@@ -1,5 +1,28 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SearchBar } from '../../components/SearchBar';
+import { Colors } from '../../theme/Colors';
+import { useState } from 'react';
 
 export function HistoryScreen() {
-  return <View />;
+  const [merchant, setMerchant] = useState<string>();
+  return (
+    <View style={styles.container}>
+      <SearchBar
+        placeholder="Search for merchant..."
+        onSearch={text => {
+          console.log(text);
+          setMerchant(text);
+        }}
+      />
+      <Text>{merchant}</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: Colors.neutral,
+  },
+});
